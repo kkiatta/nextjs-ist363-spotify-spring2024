a9c54a5731db01b2517daf82c87984237fef1d80
kkiatta <kkiattaw@syr.edu>
2024-05-02 16:45:40 -0400

    spotify api

--- app/page.js
+++ app/page.js
@@ -1,24 +1,35 @@
-import Heading from "../components/Heading";
-import Parapgraph from "../components/Paragraph";
+"use client";
 
-const Home = () => {
-  return (<main>
-    <Heading level={1}>Spotify</Heading>
-    <Heading level={2}>Artists</Heading>
-    <Heading level={3}>Songs</Heading>
-    <Parapgraph intro>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquet, sem vitae cursus vehicula, felis nibh placerat metus, in mollis leo est non tellus. Ut dapibus lobortis nisl at posuere. Sed ut dapibus elit. Aliquam elementum commodo gravida. Nunc rutrum purus non mi dapibus scelerisque. Etiam metus quam, pharetra vel ullamcorper sed, venenatis ac elit. In hac habitasse platea dictumst. Ut a dolor tristique, malesuada tellus id, viverra dolor. Donec porttitor mollis orci, vel tristique ipsum consectetur euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-    </Parapgraph>
-    <Parapgraph marginBottom={1}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquet, sem vitae cursus vehicula, felis nibh placerat metus, in mollis leo est non tellus. Ut dapibus lobortis nisl at posuere. Sed ut dapibus elit. Aliquam elementum commodo gravida. Nunc rutrum purus non mi dapibus scelerisque. Etiam metus quam, pharetra vel ullamcorper sed, venenatis ac elit. In hac habitasse platea dictumst. Ut a dolor tristique, malesuada tellus id, viverra dolor. Donec porttitor mollis orci, vel tristique ipsum consectetur euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-    </Parapgraph>
-    <Parapgraph marginBottom={5}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquet, sem vitae cursus vehicula, felis nibh placerat metus, in mollis leo est non tellus. Ut dapibus lobortis nisl at posuere. Sed ut dapibus elit. Aliquam elementum commodo gravida. Nunc rutrum purus non mi dapibus scelerisque. Etiam metus quam, pharetra vel ullamcorper sed, venenatis ac elit. In hac habitasse platea dictumst. Ut a dolor tristique, malesuada tellus id, viverra dolor. Donec porttitor mollis orci, vel tristique ipsum consectetur euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-    </Parapgraph>
-    <Parapgraph caption>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquet, sem vitae cursus vehicula, felis nibh placerat metus, in mollis leo est non tellus. Ut dapibus lobortis nisl at posuere. Sed ut dapibus elit. Aliquam elementum commodo gravida. Nunc rutrum purus non mi dapibus scelerisque. Etiam metus quam, pharetra vel ullamcorper sed, venenatis ac elit. In hac habitasse platea dictumst. Ut a dolor tristique, malesuada tellus id, viverra dolor. Donec porttitor mollis orci, vel tristique ipsum consectetur euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-    </Parapgraph>
-  </main>
-  );
-};
-export default Home;
+import { useEffect, useState } from "react";
+
+function HomePage() {
+ const [artists, setArtist] = useState([]);
+ const [isLoading, setIsLoading] = useState(true);
+ const [error, setError] = useState(null);
+
+ useEffect(() => {
+  try {
+    fetch("/api/artists")
+     .then((res) => res.json())
+     .then((data) => { 
+      setArtist(data.artists);
+      setIsLoading(false);
+     });
+   } catch (error) {
+    setError(error);
+   }
+ }, []);
+
+ if (isLoading) return <p>Loading...</p>;
+ if (error) return <p>Error loading artists</p>;
+
+ console.log({artists});
+
+ return (
+  <div>{artists.map((artist) => {
+    return <h1 key={artist.id}>{artist.name}</h1>
+  })}</div>
+ );
+}
+
+export default HomePage;
